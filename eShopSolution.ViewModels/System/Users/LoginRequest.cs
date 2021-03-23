8e30eeb9852a8a9c1f9b02ce58c5301df86654a7
Huy Ho <ho.an.huy@live.com>
2021-03-24 01:04:09 +0700

    Using fluent validator

--- eShopSolution.ViewModels/System/Users/LoginRequest.cs
+++ eShopSolution.ViewModels/System/Users/LoginRequest.cs
@@ -1,11 +1,23 @@
-﻿namespace eShopSolution.ViewModels.System.Users
+﻿using FluentValidation;
+
+namespace eShopSolution.ViewModels.System.Users
 {
     public class LoginRequest
     {
         public string UserName { get; set; }
 
-        public string Passwrod { get; set; }
+        public string Password { get; set; }
 
         public bool RememberMe { get; set; }
+    }
+
+    public class LoginRequestValidator: AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("User name is required");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required")
+                .MinimumLength(6).WithMessage("Password is at least 6 characters");
+        }
     }
 }
